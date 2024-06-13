46ab14452d7cc7520f89520f476ab5c597e3535b
Wang Zheng <ufo5260987423@163.com>
2024-06-13 13:43:41 +0800

    fix

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+source .akku/bin/activate
+
+skip=(
+    # "./tests/output-identifier-types.sps" 
+    # "./tests/parallel-log-debug.sps" 
+    # "./tests/log-debug.sps" 
+)
+
+success=0
+
+for test in $(find ./tests | grep ".sps$")
+do
+    if [[ "${skip[@]}" =~ $test ]]; then continue; fi
+    scheme --quiet $test
+    success=$(($? || $success))
+done;
+
+exit $success
